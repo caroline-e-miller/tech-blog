e8de226315d02bf29911f43ce80820da203b45e5
caroline-e-miller <caroline.miller@ymail.com>
2021-04-12 12:23:54 -0500

    fixed signup, added comment seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,14 +1,19 @@
 const sequelize = require('../config/connection');
-const { User, Blog } = require('../models');
+const { User, Blog, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
     const users = await User.bulkCreate(userData, {
         individualHooks: true,
+        returning: true,
+    });
+
+    await Comment.bulkCreate(commentData, {
         returning: true,
     });
 
