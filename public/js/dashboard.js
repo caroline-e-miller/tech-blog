142114693403f8fe93bb76476512ce0001b43366
Caroline Miller <caroline.miller@ymail.com>
2021-07-26 14:30:25 -0500

    removed unneccessary comments

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,32 +21,6 @@ const newBlog = async (event) => {
     }
 };
 
-
-// const updateBlog = async (event) => {
-//     event.preventDefault();
-
-//     const title = document.querySelector('#blog-name').value.trim();
-//     const post_body = document.querySelector('#blog-body').value.trim();
-//     const id = event.target.getAttribute('blog-edit-id');
-
-//     console.log(`### ${title} ### ${post_body} ## ${id}`);
-
-//     if (title && post_body) {
-//         // Send the e-mail and password to the server
-//         const response = await fetch(`/api/post/${id}`, {
-//             method: 'PUT',
-//             body: JSON.stringify({ title, post_body }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
-
-//         if (response.ok) {
-//             document.location.replace(`/dashboard`);
-//         } else {
-//             alert('Failed to update the blog.');
-//         }
-//     }
-// }
-
 const deleteBlog = async (event) => {
     if (event.target.hasAttribute('blog-data-id')) {
         const id = event.target.getAttribute('blog-data-id');
@@ -71,6 +45,4 @@ document
     .querySelector('.blog-list')
     .addEventListener('click', deleteBlog);
 
-// document
-//     .querySelector('.edit-btn')
-//     .addEventListener('click', updateBlog);
+
